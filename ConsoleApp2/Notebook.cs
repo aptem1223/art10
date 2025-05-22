5963426599b6d1b918a26bb43c81d169d32e07d3
aptem1223 <sanstop357@gmail.com>
2025-05-22 10:51:36 +0300

    Add files via upload

---
+++ ConsoleApp2/Notebook.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    struct Notebook
+    {
+        public string Model;
+        public string Manufacturer;
+        public decimal Price;
+        public Notebook(string model, string manufacturer, decimal price)
+        {
+            Model = model;
+            Manufacturer = manufacturer;
+            Price = price;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Информация о ноутбуке:");
+            Console.WriteLine($"Модель: {Model}");
+            Console.WriteLine($"Производитель: {Manufacturer}");
+            Console.WriteLine($"Цена: {Price} руб.");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Notebook notebook = new Notebook("XPS 13", "Dell", 129999.99m);
+            notebook.DisplayInfo();
+            Console.ReadLine(); 
+        }
+    }
+}
