09570a63f3596dbfe1ad77318bae81a869e77677
aptem1223 <sanstop357@gmail.com>
2025-05-22 10:57:15 +0300

    Add files via upload

---
+++ ConsoleApp3/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    struct Book
+    {
+        public string Title;
+        public string AuthorFullName;
+        public int Year;
+        public int Pages;
+        public Book(string title, string authorFullName, int year, int pages)
+        {
+            Title = title;
+            AuthorFullName = authorFullName;
+            Year = year;
+            Pages = pages;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Информация о книге:");
+            Console.WriteLine($"Название: {Title}");
+            Console.WriteLine($"Автор: {AuthorFullName}");
+            Console.WriteLine($"Год издания: {Year}");
+            Console.WriteLine($"Количество страниц: {Pages}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество книг: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Book[] books = new Book[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"\nВведите данные для книги #{i + 1}:");
+
+                Console.Write("Название: ");
+                string title = Console.ReadLine();
+
+                Console.Write("Ф.И.О. автора: ");
+                string author = Console.ReadLine();
+
+                Console.Write("Год издания: ");
+                int year = int.Parse(Console.ReadLine());
+
+                Console.Write("Количество страниц: ");
+                int pages = int.Parse(Console.ReadLine());
+
+                books[i] = new Book(title, author, year, pages);
+            }
+            Console.WriteLine("\nСписок всех книг:");
+            foreach (Book book in books)
+            {
+                book.DisplayInfo();
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
